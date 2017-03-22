e8c9fad205c424b084cd57fc5533901b10eadc96
khaled hossain <md.khaled.eee@gmail.com>
2017-03-22 11:05:29 +0600

    Add clean style files

--- gulpFile.js
+++ gulpFile.js
@@ -1,12 +1,34 @@
+const chalk = require('chalk'),
+      log = console.log,
+      error = chalk.bold.red,
+      info = chalk.bgBlue;
 var gulp = require('gulp'),
     args = require('yargs').argv,
+    del = require('del'),
     $ = require('gulp-load-plugins')({lazy:true});
 
 gulp.task('js', function(){
+    log(info('JS task starting'));
     return gulp
         .src([
             './src/**/*.js',
             './*.js'
         ])
         .pipe($.if(args.verbose,$.print()));
+});
+
+gulp.task('styles', function(){
+    log(info('Compiling less to css'));
+    return gulp
+        .src([
+            './src/client/styles/*.less'
+        ])
+        .pipe($.less())
+        .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
+        .pipe(gulp.dest('./.temp'))
+});
+
+gulp.task('clean-styles', function(){
+    var files = './.temp/*.css';
+    del(files);
 })
